05acc3d08678dd11625cfa951867fe829b5dc881
Seth Pipho <seth.pipho@gmail.com>
2019-07-21 18:02:13 -0400

    hybrid parsing approach, lark for quanity-unit, regex for rest

--- parsing.py
+++ parsing.py
@@ -21,13 +21,13 @@ def list_to_grammar(lst):
     return "|".join(strs)
 
 
-
 standard_units = list_from_file(os.path.join(script_path, 'data/standard-units.txt'))
+non_standard_units = list_from_file(os.path.join(script_path, 'data/non-standard-units.txt'))
 
 _GRAMMAR_ =  '''
 ?start: ingredient
 
-ingredient: quantity? unit? base_ingredient
+ingredient: quantity? unit?  base_ingredient
 
 quantity.2: mixed_fraction | fraction | number
 
@@ -40,22 +40,22 @@ standard_unit.2: standard_unit_plural | STANDARD_UNIT_NAME
 ?standard_unit_plural.2:  STANDARD_UNIT_NAME ("es" | "s")
 STANDARD_UNIT_NAME: " " ({standard_units})
 
-non_standard_unit.2: unit_size?  (non_standard_unit_plural | " " NON_STANDARD_UNIT_NAME)
-?non_standard_unit_plural.2: " " NON_STANDARD_UNIT_NAME ("es" | "s")
-NON_STANDARD_UNIT_NAME: "can" | "jar" | "package"
+non_standard_unit.2: unit_size?  (non_standard_unit_plural |  NON_STANDARD_UNIT_NAME)
+?non_standard_unit_plural.2: NON_STANDARD_UNIT_NAME ("es" | "s")
+NON_STANDARD_UNIT_NAME: " " ({non_standard_units})
 unit_size: " (" quantity standard_unit ")"
 
 base_ingredient.0: WORD+
 
 number: NUMBER
-WORD: /[A-Z a-z \-]+/
+WORD: /[A-Z,a-z \-]+/
 
 %import common.NUMBER 
 %import common.WS
 
 
 
-'''.format(standard_units=list_to_grammar(standard_units))
+'''.format(standard_units=list_to_grammar(standard_units), non_standard_units=list_to_grammar(non_standard_units))
 
 parser = Lark(_GRAMMAR_)
 
@@ -79,9 +79,9 @@ class IngredientTransformer(Transformer):
     
     def non_standard_unit(self, children):
         if len(children) == 2:
-            return {"type": "non_standard", "size": children[0], "name": children[1].value}
+            return {"type": "non_standard", "size": children[0], "name": children[1].value.strip()}
         if len(children) == 1:
-            return {"type": "non_standard", "name": children[0].value}
+            return {"type": "non_standard", "name": children[0].value.strip()}
 
     def unit_size(self, children):
         return {"quantity": children[0].children[0], "unit": children[1]}
@@ -105,11 +105,12 @@ class IngredientTransformer(Transformer):
 def parse_ingredient(string):
     ''' parses ingredient to structured dict '''
     tree = parser.parse(string)
+
     return IngredientTransformer().transform(tree)
 
 
 if __name__ == "__main__":
-    tree = parser.parse("10 (6 oz) cans salt")
+    tree = parser.parse("10 jar sauce")
     print(tree)
     print(tree.pretty())
     tree = IngredientTransformer().transform(tree)
